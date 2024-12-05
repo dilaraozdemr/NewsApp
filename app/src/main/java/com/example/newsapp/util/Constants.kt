353aedb280e3a6736912af834220803f6352a95f
Dilara Özdemir <73308194+dilaraozdemr@users.noreply.github.com>
2024-12-05 15:46:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "06f21eb9158045fa90081c86cee04635"
+        const val BASE_URL = "https://newsapi.org/"
+        const val  SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
